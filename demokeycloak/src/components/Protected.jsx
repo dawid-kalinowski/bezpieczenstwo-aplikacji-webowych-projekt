51142032a02ba56c49764c1445f92d13827e19c3
Dawid Kalinowski <dawidkalinowski243@gmail.com>
2024-06-22 20:34:07 +0200

    dane wyswietalaj sie po strone klienta

--- demokeycloak/src/components/Protected.jsx
+++ demokeycloak/src/components/Protected.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import axios from "axios";
 
 const Protected = () => {
+    const isRun = useRef(false);
+
+    useEffect(() => {
+        if (isRun.current) return;
+
+        isRun.current = true;
+
+        axios
+        .get("/documents")
+        .then((res) => console.log(res.data))
+        .catch((err) => console.error(err));
+    }, []);
   return <div>Protected</div>;
 };
 
