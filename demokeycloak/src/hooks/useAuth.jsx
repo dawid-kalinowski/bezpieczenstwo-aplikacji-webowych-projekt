045d94e678921a7b6156a974682fd6e7b690b8fe
Dawid Kalinowski <dawidkalinowski243@gmail.com>
2024-06-22 17:56:20 +0200

    polaczenie z keycloak

--- demokeycloak/src/hooks/useAuth.jsx
+++ demokeycloak/src/hooks/useAuth.jsx
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Keycloak } from "keycloak-js";
 
 const useAuth = () => {
     const [isLogin, setLogin] = useState(false);
+
+    useEffect(() => {
+        const client = new Keycloak( {
+            url: import.meta.env.VITE_KEYCLOAK_URL,
+            realm: import.meta.env.VITE_KEYCLOAK_REALM,
+            clientID: import.meta.env.VITE_KEYCLOAK_CLIENT,
+        });
+        client.init({ onLoad: "login-required"}).then((res) => setLogin(res));
+    }, []);
+
     return isLogin;
 }
 export default useAuth;
